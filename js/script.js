6d962382db3eb463df67a2e2724e2b28855830d2
Olga Bezginova <bezginova.olga@gmail.com>
2018-08-06 14:37:24 +0300

    Set image attributes

--- js/script.js
+++ js/script.js
@@ -3,10 +3,16 @@ const input = document.createElement('input');
 const div   = document.createElement('div');
 
 const imgNum = 5;
+const falls = ['Angel','Kaieteur', 'Sutherland', 'Victoria', 'Yosemite'];
 
 for(let i = 0; i < imgNum; i++){
     const newImg  = document.createElement('img'); 
-    newImg.setAttribute('src', `img/img${i + 1}.jpg`);    
+    newImg.setAttribute('src', `img/img${i + 1}.jpg`); 
+    newImg.setAttribute('alt', falls[i]);
+    newImg.dataset.name = falls[i];
     div.appendChild(newImg);
 }
 
+
+
+//document.body.appendChild(div);
